1a64e543a0b28fedba77e0b28cff19128945fb77
baluyotraf <baluyotraf@outlook.com>
2017-10-25 14:47:36 +0800

    initial commit

---
+++ backend/app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/api/")
+def api_hello():
+    return "API: Hello World!"
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', debug=True)
